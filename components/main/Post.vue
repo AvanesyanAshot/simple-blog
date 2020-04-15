6a6f73a41bb0d8c0c4885a33be5f3e5a27971809
Ashot Avanesyan <9teron9@gmail.com>
2020-04-15 20:09:56 +0300

    add post

---
+++ components/main/Post.vue
@@ -0,0 +1,63 @@
+<template>
+    <el-card
+        shadow='hover'
+        :body-style="{padding: 0}"
+        class='post'
+    >
+        <header slot="header" class="post-header">
+            <h3>header</h3>
+
+            <small>{{ new Date().toLocaleString() }}</small>
+        </header>
+        <div class="post-body">
+            <img src="https://avatars.mds.yandex.net/get-pdb/2883918/1938b612-fbe0-43bc-bc82-e5d90e533f42/s1200?webp=false" 
+                alt="post" 
+                class='post-img'
+            >
+        </div>
+        <footer class='post-footer'>
+            <el-button size="small" @click="openPost">Открыть</el-button>
+
+            <span><i class='el-icon-message'></i>12</span>
+        </footer>
+    </el-card>
+</template> 
+
+<script>
+
+export default {
+    methods: {
+        openPost() {
+            alert('Открыл')
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .post {
+        margin-bottom: 1.5rem;
+    }
+    
+    .post-header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .post-body{
+        margin-bottom: 1rem;
+    }
+
+    .post-img{
+        width: 100%;
+        height: auto;
+    }
+
+    .post-footer{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="container">
-    <h1>This home page</h1>
-  </div>
+ <el-row type='flex' justify="center">
+   <el-col  :xs='24' :sm='18' :md='12' :lg="10">
+     <AppPost  v-for='post in 3' :key="post" />
+   </el-col>
+ </el-row>
 </template>
 
 <script>
+import AppPost from '@/components/main/Post'
 
 
 export default {
-  
+  head: {
+    title: 'Главная'
+  },
+  components:{
+    AppPost
+  }
 }
 </script>
 
