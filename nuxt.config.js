0a5c1a9c63c321892b0d3fffd48ca7306fe1a1f2
Ashot Avanesyan <9teron9@gmail.com>
2020-04-15 14:29:08 +0300

    add express server

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   mode: 'universal',
 
   head: {
-    title: 'Simple_Blog',
+    title: 'Nuxt lesson',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -21,7 +21,7 @@ export default {
   ],
 
   plugins: [
-    '@/plugins/element-ui'
+    '@/plugins/globals'
   ],
 
   buildModules: [
