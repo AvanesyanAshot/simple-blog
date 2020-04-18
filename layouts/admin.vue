aa8efc86b2333d1ae9c9f7b4583caf8dd738aaeb
Ashot Avanesyan <9teron9@gmail.com>
2020-04-18 09:57:19 +0300

    add admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="admin-layout-wrap">
+        <el-container :style='{height:"100%"}'>
+            <el-aside width="200px">
+                Aside
+            </el-aside>
+            <el-main>
+                <nuxt />
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .admin-layout-wrap {
+        width: 100%;
+        height: 100vh;
+    }
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Welcome to admin</h1>
+</template>
+
+<script>
+export default {
+    layout: 'admin'
+}
+</script>
