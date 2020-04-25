1b3fffc6c31812e716b9a0e9beb9d958da8a761a
Ashot Avanesyan <9teron9@gmail.com>
2020-04-25 12:49:33 +0300

    file manage

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const keys = require('./keys')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json)
+
+module.exports = app
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const consola = require('consola')
 const {Nuxt, Builder} = require('nuxt')
-const app = express()
+const app = require('./app')
 
-let config = require('../nuxt.config.js')
+const config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
 async function start() {
@@ -21,11 +21,13 @@ async function start() {
 
     app.use(nuxt.render)
 
-    app.listen(port, host)
-    consola.ready({
-        message: `Server listening on http://${host}:${port}`,
-        badge: true
+    app.listen(port, host, () =>{
+        consola.ready({
+            message: `Server listening on http://${host}:${port}`,
+            badge: true
+        })
     })
+
 }
 
 
